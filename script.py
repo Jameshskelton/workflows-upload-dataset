26d63e7d10fef0a141eb2e3c7a24d8cb53b444c8
jameshskelton <jameshskelton@gmail.com>
2022-07-13 19:07:04 -0500

    update

--- script.py
+++ script.py
@@ -24,6 +24,6 @@ model, params = DalleBart.from_pretrained(DALLE_MODEL, revision=DALLE_COMMIT_ID,
 vqgan, vqgan_params = VQModel.from_pretrained(VQGAN_REPO, revision=VQGAN_COMMIT_ID, _do_init=False)
 print('stage 3')
 
-model.save('dallebart/', params = params)
+model.save_pretrained('dallebart/', params = params)
 vqgan.save_pretrained('vqgan-jax/',params=vqgan_params)
 
